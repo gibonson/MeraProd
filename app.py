63e58e08567b3db8d4e000f13fce6fa58fbf6937
gibonson <jakubpalica@gmail.com>
2022-05-16 00:29:24 +0200

    form2

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template, request, jsonify ,redirect, url_for
+from flask import Flask, render_template, request, jsonify, redirect, url_for, flash, request
+from wtforms import Form, TextAreaField, validators, StringField, SubmitField
 from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow 
+from flask_marshmallow import Marshmallow
 import os
-
-
+import datetime
 
 # Init app
 app = Flask(__name__)
 baseDir = os.path.abspath(os.path.dirname(__file__))
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 
 # DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(baseDir, 'db.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(baseDir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
@@ -32,21 +35,25 @@ class Product(db.Model):
     numberOfParts = db.Column(db.Integer)
     bracket = db.Column(db.Boolean)
     singleOrDouble = db.Column(db.Integer)
+    orderStatus = db.Column(db.Integer)
+    executionDate = db.Column(db.DateTime)
 
-    def __init__(self, belegNumber, modelName,lenght, numberOfParts, bracket, singleOrDouble):
+    def __init__(self, belegNumber, modelName, lenght, numberOfParts, bracket, singleOrDouble, orderStatus, executionDate):
         self.belegNumber = belegNumber
         self.modelName = modelName
         self.lenght = lenght
-        self.numberOfParts =numberOfParts
+        self.numberOfParts = numberOfParts
         self.bracket = bracket
         self.singleOrDouble = singleOrDouble
+        self.orderStatus = orderStatus
+        self.executionDate = executionDate
 
 
 # Product Schema
 class ProductSchema(ma.Schema):
-  class Meta:
-        fields = ('id','belegNumber','modelName','lenght','numberOfParts','bracket','singleOrDouble')
-
+    class Meta:
+        fields = ('id', 'belegNumber', 'modelName', 'lenght',
+                  'numberOfParts', 'bracket', 'singleOrDouble', 'orderStatus', 'executionDate')
 
 
 # Init schema
@@ -55,10 +62,8 @@ product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
 
 
-
-
-#API Create a Product
-@app.route('/product', methods = ['POST'])
+# API Create a Product
+@app.route('/product', methods=['POST'])
 def addProduct():
     belegNumber = request.json['belegNumber']
     modelName = request.json['modelName']
@@ -66,15 +71,18 @@ def addProduct():
     numberOfParts = request.json['numberOfParts']
     bracket = request.json['bracket']
     singleOrDouble = request.json['singleOrDouble']
+    orderStatus = request.json['orderStatus']
+    executionDate = request.json['executionDate']
 
-    newProduct = Product(belegNumber, modelName, lenght, numberOfParts, bracket, singleOrDouble)
+    newProduct = Product(belegNumber, modelName, lenght,
+                         numberOfParts, bracket, singleOrDouble, orderStatus, executionDate)
     db.session.add(newProduct)
     db.session.commit()
 
     return product_schema.jsonify(newProduct)
 
 
-#API Get All Products
+# API Get All Products
 @app.route('/product', methods=['GET'])
 def get_products():
     all_products = Product.query.all()
@@ -82,15 +90,15 @@ def get_products():
     return jsonify(result)
 
 
-#API Get Product
+# API Get Product
 @app.route('/product/<id>', methods=['GET'])
 def get_product(id):
     product = Product.query.get(id)
     return product_schema.jsonify(product)
 
 
-#API Update a Product
-@app.route('/product/<id>', methods = ['PUT'])
+# API Update a Product
+@app.route('/product/<id>', methods=['PUT'])
 def updateProduct(id):
 
     product = Product.query.get(id)
@@ -101,6 +109,8 @@ def updateProduct(id):
     numberOfParts = request.json['numberOfParts']
     bracket = request.json['bracket']
     singleOrDouble = request.json['singleOrDouble']
+    orderStatus = request.json['orderStatus']
+    executionDate = request.json['executionDate']
 
     product.belegNumber = belegNumber
     product.modelName = modelName
@@ -108,13 +118,15 @@ def updateProduct(id):
     product.numberOfParts = numberOfParts
     product.bracket = bracket
     product.singleOrDouble = singleOrDouble
+    product.orderStatus = orderStatus
+    product.executionDate = executionDate
 
     db.session.commit()
 
     return product_schema.jsonify(product)
 
 
-#API Delete Product
+# API Delete Product
 @app.route('/product/<id>', methods=['DELETE'])
 def del_product(id):
     product = Product.query.get(id)
@@ -123,10 +135,71 @@ def del_product(id):
     return product_schema.jsonify(product)
 
 
-#GUI
+# GUI
 @app.route('/')
 def hello_world():
     return render_template('home.html')
+
+
+@app.route('/getTable')
+def getTable():
+    all_products = Product.query.all()
+    print(all_products)
+    for column in all_products:
+        print("% s % s" % (column.id, column.lenght))
+    return render_template('table.html', all_products=all_products)
+
+
+@app.route('/remove', methods=['GET', 'POST'])
+def remove():
+    if request.method == 'POST':
+        id = request.form['id']
+        product = Product.query.get(id)
+        db.session.delete(product)
+        db.session.commit()
+    return redirect(url_for('getTable'))
+
+
+messages = [{'title': 'Message One',
+             'content': 'Message One Content'},
+            {'title': 'Message Two',
+             'content': 'Message Two Content'}
+            ]
+
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    messages.clear()
+    if request.method == 'POST':
+        belegNumber = request.form['belegNumber']
+        modelName = request.form['modelName']
+        lenght = request.form['lenght']
+        numberOfParts = request.form['numberOfParts']
+        bracket = request.form['bracket']
+        if bracket == "True": bracket = True
+        if bracket == "False": bracket = False
+        singleOrDouble = request.form['singleOrDouble']
+        orderStatus = request.form['orderStatus']
+        executionDate = request.form['executionDate']
+        format = "%Y-%m-%d"
+        executionDate = datetime.datetime.strptime(executionDate, format)
+
+        if not belegNumber:
+            flash('belegNumber is required!')
+        elif not modelName:
+            flash('modelName is required!')
+        else:
+            # flash(title + " " + content + " added")
+            messages.append({'title': belegNumber, 'content': modelName})
+            # # return redirect(url_for('getTable'))
+            # # return redirect(url_for('getTable'))
+            newProduct = Product(belegNumber, modelName, lenght, numberOfParts, bracket, singleOrDouble, orderStatus, executionDate)
+            db.session.add(newProduct)
+            db.session.commit()
+            return render_template('create.html', messages=messages)
+
+
+    return render_template('create.html', messages=messages)
 
 
 @app.route('/help')
