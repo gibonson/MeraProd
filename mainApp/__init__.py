7bea30f1e8e4fbf6b881aee280651909d0677628
gibonson <jakubpalica@gmail.com>
2023-04-24 01:01:59 +0200

    update

--- mainApp/__init__.py
+++ mainApp/__init__.py
@@ -10,7 +10,7 @@ import os
 
 # Init app
 app = Flask(__name__, static_folder='../static')
-baseDir = os.path.abspath(os.path.dirname(__file__))   + "\\..\\userFiles"
+baseDir = os.path.abspath(os.path.dirname(__file__))   + "//..//userFiles"
 print(baseDir)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
